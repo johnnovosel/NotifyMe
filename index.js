ab08db66038857aa11adbf6a8f7ea3f33de2659c
Karatekk2 <johnnovosel96@gmail.com>
2017-05-27 00:06:26 -0500

    moved add command to its own js file

--- index.js
+++ index.js
@@ -1,82 +1,23 @@
-var isChannel = require('is-youtube-channel'),
-    config = require('./config'),
+var config = require('./config'),
     User = require('./lib/user.js'),
     Channel = require('./lib/Channels'),
     channelVids = require('yt-channel-videos')(process.env.YOUTUBE_KEY),
+    Add = require('./lib/commands/AddSub.js'),
     Discord = require('discord.js');
 
 // required variables for commands
 var userArray = [];
-var channelArray = [];
-var channelNum = 0;
 
 // list of commands
-var commands = new Map([
+var commandsMap = new Map([
     ['ls', function () {
 
     }],
-    ['add', function (space, message, username) {
-        let channelName = message.content.substring(space + 1).toLowerCase();
-
-        isChannel(channelName, function (err, valid) {
-            if (err) {
-                console.log("Error");
-            } else {
-                if (valid == true) {
-                    console.log('valid channel');
-
-                    if (channelNum === 0) {
-                        let channel = new Channel(channelName);
-                        channelArray.push(channel);
-                        channelArray[0].addSub(username);
-                        channelArray[0].updateVideoID(channelName);
-                        message.reply('Added ' + channelName + ' to your subscriptions');
-                        channelNum++;
-                    } else {
-
-                        let found = false;
-                        for (var i = 0; i < channelNum; i++) {
-                            if (channelNum > 0 && channelName === channelArray[i].getChannelName()) {
-                                channelArray[i].addSub(username);
-                                IncUserList(username, channelName);
-                                message.reply('Added ' + channelName + ' to your subscriptions');
-                                found = true;
-                                break;
-                            }
-                        }
-
-                        if (!found) {
-                            let channel = new Channel(channelName);
-                            channelArray.push(channel);
-                            channelNum++;
-                            channelArray[channelNum - 1].addSub(username);
-                            channelArray[0].updateVideoID(channelName);
-                            message.reply('Added ' + channelName + ' to your subscriptions');
-                            IncUserList(username, channelName);
-                        }
-                    }
-                    function IncUserList(username, channelName) {
-                        for (var i = 0; i < userArray.length; i++) {
-                            if (username == userArray[i].getUsername) {
-                                userArray[i].incCount();
-                                userArray[i].addSub(channelName);
-                            }
-                        }
-                    }
-                }
-                else {
-                    message.reply('This youtube channel does not exist');
-                }
-            }
-        });
-
-    }],
+    ['add', function (space, message, userObject) { Add.Command(space, message, userObject) }],
     ['create', function (space, message, username) {
         const channel = bot.channels.find('name', 'bot-commands');
         let person = new User(username);
         channel.send(`Welcome to NotifyBot, ${username}`);
-
-        userArray.push(person);
     }]
 ]);
 
@@ -93,8 +34,6 @@ bot.on('ready', function () {
     } else {
         bot.user.setStatus('Online').catch(err => console.error(err));
         console.log('status set to online');
-        let username = "168216574052925440";
-        
     }
 });
 
@@ -110,13 +49,14 @@ function doCommand(message) {
     if (space === -1) {
         space = message.content.length;
     }
+
     let cmd = message.content.substring(1, space).toLowerCase();
-    let username = message.author;
+    let userObject = message.author;
 
     let found = false;
-    for (let key of commands.keys()) {
+    for (let key of commandsMap.keys()) {
         if (cmd === key) {
-            commands.get(cmd)(space, message, username);
+            commandsMap.get(cmd)(space, message, userObject);
             console.log('good command');
             found = true
         }
@@ -131,20 +71,26 @@ function doCommand(message) {
 bot.login(config.discordToken);
 
 async function checkVidNum(channel) {
-    let lastVidID = (await channelVids.allUploads(channelName)).items[0].id;
+    console.log('b lastvidid');
+    let lastVidID = (await channelVids.allUploads(channel.getChannelName())).items[0].id;
+    console.log('a lastvidid');
+
     if (lastVidID != channel.lastVidID) {
-        for(var i = 0; i < channel.subscriberList.length; i++) {
+        for (var i = 0; i < channel.subscriberList.length; i++) {
             const guildChannel = bot.channels.find('name', 'bot-commands');
+
             user = guildChannel.subscriberList[i];
             guildChannel.send(guildChannel.channelName + ` has a new video, ${user}`);
         }
     }
 }
 
-setInterval(function() {
+setInterval(function () {
     console.log('this is check subs');
-    for (var i = 0; i < channelArray.length; i++) {
-        checkVidNum(channelArray[i]);
+    console.log(Add.ChannelArray.length);
+    for (var i = 0; i < Add.ChannelArray.length; i++) {
+        checkVidNum(Add.ChannelArray[i]).catch(err => console.error(err));
     }
-}, 1000*60*15);
+}, 1000 * 60 * 15);
+
 
